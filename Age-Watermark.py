7a0f4d1c4b1e700d25756508224c9bcba0d2fa50
lhw828 <lhw828@gmail.com>
2023-04-07 13:27:11 +0800

    Bump to 1.3
    改进：
    1、支持处理子目录图片
    2、处理后统一输出到D:\\已处理\\
    3、新增程序运行后手动输入小孩生日

--- Age-Watermark.py
+++ Age-Watermark.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageDraw, ImageFont
 from PIL.ExifTags import TAGS
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 import os
 import msvcrt #只适用于Windows程序实现按任意键退出，Linux和macOS需要sys,tty,termios和其他的代码
 
@@ -27,18 +27,19 @@ def get_date_taken(image):
     return None
 
 # 处理图像
-def process_image(image_path, output_dir):
+def process_image(image_path, output_dir, target_date):
     image = Image.open(image_path)
     date_taken = get_date_taken(image)
     if not date_taken:
         return
 # 指定目标日期（例如小孩的生日）
-    target_date = datetime.strptime('2010-05-11', '%Y-%m-%d').date()
-    # 计算天数、月数和年数 
+    target_date = datetime.strptime('2020-03-21', '%Y-%m-%d').date()
+    formatted_date = target_date.strftime('%Y-%m-%d')
+    # 计算天数、月数和年数
     delta = date_taken - target_date
     days = delta.days
     if days < 0:
-        text = '来自未来的照片'
+        text = f'{formatted_date}之前的照片'
     elif days <= 100:
         text = f'{days} 天'
     elif days < 365:
@@ -73,25 +74,32 @@ def process_image(image_path, output_dir):
     draw.text((x, y1), str(date_taken), fill='magenta', font=font)
     draw.text((x, y2), text, fill='magenta', font=font)
     # 将处理后的图像保存到指定的输出目录
-    output_path = os.path.join(output_dir, os.path.basename(image_path))
+    output_path = os.path.join(output_dir, os.path.relpath(image_path, input_dir))
+    output_dir = os.path.dirname(output_path)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
     image.save(output_path,quality=95)
 
-# 处理指定目录中的所有图像
-def process_images(input_dir, output_dir):
-    for root, dirs, files in os.walk(input_dir):
-        for file in files:
-            if file.endswith(('.jpg', '.jpeg', '.JPG', '.JPEG')):
-                image_path = os.path.join(root, file)
-                print(f"正在处理图片: {file}")
-                process_image(image_path, output_dir)
 
-#指定要处理的图片所在目录
-input_dir = 'D:\\test\\'
-#指定处理完的图片存储目录
-output_dir = 'D:\\test\\已处理\\'
+while True:
+    date_str = input("请输入一个日期（比如小孩的生日），格式为:2020-03-21:")
+    try:
+        target_date = datetime.strptime(date_str, '%Y-%m-%d').date()
+        break
+    except ValueError:
+        print("输入格式不正确，请重新输入")
+
+input_dir = os.getcwd()
+output_dir = 'D:\\已处理\\'
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
-process_images(input_dir, output_dir)
 
-print("已处理完毕，按任意键退出")
+for root, dirs, files in os.walk(input_dir):
+    for file in files:
+        if file.endswith(('.jpg', '.jpeg', '.JPG', '.JPEG')):
+            image_path = os.path.join(root, file)
+            print(f"正在处理图片: {file}")
+            process_image(image_path, output_dir, target_date)
+
+print("完毕，已处理的图片存放在“D:\已处理”文件夹，请查看。按任意键退出")
 msvcrt.getch()
