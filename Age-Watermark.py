56ec68ab970ba2ff769886c2a98fc8a6ea35f092
lhw828 <lhw828@gmail.com>
2023-04-09 03:23:12 +0800

    Bump to V1.5
    改动：
    1、优化代码结构和逻辑；
    2、将日期输入格式从“年-月-日”改为“年月日”，减小输入量；
    3、字号由固定大小改为根据图片大小自适应。

--- Age-Watermark.py
+++ Age-Watermark.py
@@ -25,81 +25,79 @@ def get_date_taken(image):
         date_taken = datetime.strptime(date_string, '%Y:%m:%d %H:%M:%S')
         return date_taken.date()
     return None
-
-# 处理图像
-def process_image(image_path, output_dir, target_date):
-    image = Image.open(image_path)
-    date_taken = get_date_taken(image)
-    if not date_taken:
-        return
-# 指定目标日期（例如小孩的生日）
-    target_date = datetime.strptime('2020-03-21', '%Y-%m-%d').date()
-    formatted_date = target_date.strftime('%Y-%m-%d')
-    # 计算天数、月数和年数
-    delta = date_taken - target_date
-    days = delta.days
-    if days < 0:
-        text = f'{formatted_date}之前的照片'
-    elif days <= 100:
-        text = f'{days}天'
-    elif days < 365:
-        months = days // 30
-        days = days % 30
-        if days == 0:
-            text = f'{months}个月'
+      
+# 处理图像 
+def process_image(image_path, output_dir, target_date, font=None):
+    with Image.open(image_path) as image:
+        date_taken = get_date_taken(image)
+        if not date_taken:
+            return
+        img_width, img_height = image.size
+        longside = max(img_height, img_width)
+        font_size = int(longside / 40) #控制字体大小，此数值越大，字体显示越小
+        target_date = datetime.strptime(target_date, '%Y-%m-%d').date()
+        formatted_date = target_date.strftime('%Y-%m-%d')
+        # 计算天数、月数和年数
+        delta = date_taken - target_date
+        days = delta.days
+        if days < 0:
+            text = f'{formatted_date}之前的照片'
+        elif days <= 100:
+            text = f'{days}天'
+        elif days < 365:
+            months = days // 30
+            days = days % 30
+            if days == 0:
+                text = f'{months}个月'
+            else:
+                text = f'{months}个月{days}天'
         else:
-            text = f'{months}个月{days}天'
-    else:
-        years = days // 365
-        months = (days % 365) // 30
-        days = (days % 365) % 30
-        if months == 0 and days == 0:
-            text = f'{years}岁'
-        elif months == 0:
-            text = f'{years}岁零{days}天'
-        elif days == 0:
-            text = f'{years}岁{months}个月'
-        else:
-            text = f'{years}岁{months}个月{days}天'
-    # 在图像上添加文本
-    width, height = image.size
-    draw = ImageDraw.Draw(image)
-    # 此处指定字体和字号，默认微软雅黑，72号 
-    font = ImageFont.truetype('msyh.ttc', 72)
-    text_bbox = font.getbbox(text)
-    text_width, text_height = text_bbox[2], text_bbox[3]
-    x = width - text_width - 80 #右侧预留80像素
-    y1 = height - text_height * 2 - 50 #下侧预留50像素
-    y2 = height - text_height - 50
-    draw.text((x, y1), str(date_taken), fill='magenta', font=font)
-    draw.text((x, y2), text, fill='magenta', font=font)
-    # 将处理后的图像保存到指定的输出目录
-    output_path = os.path.join(output_dir, os.path.relpath(image_path, input_dir))
-    output_dir = os.path.dirname(output_path)
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    image.save(output_path,quality=95)
+            years = days // 365
+            months = (days % 365) // 30
+            days = (days % 365) % 30
+            if months == 0 and days == 0:
+                text = f'{years}岁'
+            elif months == 0:
+                text = f'{years}岁零{days}天'
+            elif days == 0:
+                text = f'{years}岁{months}个月'
+            else:
+                text = f'{years}岁{months}个月{days}天'
+        # 在图像上添加文本
+        draw = ImageDraw.Draw(image)
+        if font is None:
+            font = ImageFont.truetype('msyh.ttc', size=font_size) # 此处指定字体为雅黑
+        text_bbox = font.getbbox(text)
+        text_width, text_height = text_bbox[2], text_bbox[3]
+        x = img_width - text_width - 80 #右侧预留80像素
+        y1 = img_height - text_height * 2 - 50 #下侧预留50像素
+        y2 = img_height - text_height - 50
+        draw.text((x, y1), str(date_taken), fill='magenta', font=font)
+        draw.text((x, y2), text, fill='magenta', font=font)
+        # 将处理后的图像保存到指定的输出目录
+        output_path = os.path.join(output_dir, os.path.relpath(image_path, input_dir))
+        output_dir = os.path.dirname(output_path)
+        os.makedirs(output_dir, exist_ok=True)
+        image.save(output_path, quality=95)
 
+if __name__ == '__main__':
+    while True:
+        date_str = input("请输入一个日期（比如小孩的生日），格式为:20200321:")
+        try:
+            target_date = datetime.strptime(date_str, '%Y%m%d').date().strftime('%Y-%m-%d')
+            break
+        except ValueError:
+            print("输入日期格式不正确，请重新输入")
 
-while True:
-    date_str = input("请输入一个日期（比如小孩的生日），格式为:2020-03-21:")
-    try:
-        target_date = datetime.strptime(date_str, '%Y-%m-%d').date()
-        break
-    except ValueError:
-        print("输入格式不正确，请重新输入")
+    input_dir = os.getcwd()
+    output_dir = 'D:\\已处理\\'
 
-input_dir = os.getcwd()
-output_dir = 'D:\\已处理\\'
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+    for root, dirs, files in os.walk(input_dir):
+        for file in files:
+            if file.lower().endswith(('.jpg', '.jpeg')):
+                image_path = os.path.join(root, file)
+                print(f"正在处理图片: {file}")
+                process_image(image_path, output_dir, target_date)
 
-for root, dirs, files in os.walk(input_dir):
-    for file in files:
-        if file.endswith(('.jpg', '.jpeg', '.JPG', '.JPEG')):
-            image_path = os.path.join(root, file)
-            print(f"正在处理图片: {file}")
-            process_image(image_path, output_dir, target_date)
-
-print("完毕，已处理的图片存放在“D:\已处理”文件夹，请查看。按任意键退出")
-msvcrt.getch()
+    print("完毕，已处理的图片存放在“D:\已处理”文件夹，请查看。按任意键退出")
+    msvcrt.getch() 
